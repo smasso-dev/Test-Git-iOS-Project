15700b73eea767ed95707cc0210bc2e4bb87734f
Sergi Massó <sergimh27@gmail.com>
2014-07-31 13:54:06 +0200

    added a test class

---
+++ Git Test/GTTest.h
@@ -0,0 +1,13 @@
+//
+//  GTTest.h
+//  Git Test
+//
+//  Created by Sergi Massó Hernaez on 31/7/14.
+//  Copyright (c) 2014 Sergi Massó. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GTTest : NSObject
+
+@end
---
+++ Git Test/GTTest.m
@@ -0,0 +1,13 @@
+//
+//  GTTest.m
+//  Git Test
+//
+//  Created by Sergi Massó Hernaez on 31/7/14.
+//  Copyright (c) 2014 Sergi Massó. All rights reserved.
+//
+
+#import "GTTest.h"
+
+@implementation GTTest
+
+@end
